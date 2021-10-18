e70606e9fc353e39f2d5275a6711b6dbdc9ec265
yomer <tomerg400@gmail.com>
2021-10-18 14:38:22 +0300

    all history rewrriter bugfix

--- convo_search_project/rewriters/all_histroy_rewriter.py
+++ convo_search_project/rewriters/all_histroy_rewriter.py
@@ -2,6 +2,6 @@ class AllHistoryRewriter():
     def rewrite(self,query,**ctx):
         if len(ctx['history']) == 0:
             return query
-        if 'canonical_rsp' in ctx:
+        if 'canonical_rsp' in ctx and ctx['canonical_rsp'] is not None:
             return " ".join(ctx['history']+[ctx['canonical_rsp'][-1]]+[query])
         return " ".join(ctx['history']+[query])
