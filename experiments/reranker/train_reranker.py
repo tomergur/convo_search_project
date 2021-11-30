6222de0294855743f9420a69a54f7a853dfdcf42
yomer <tomerg400@gmail.com>
2021-11-30 11:44:35 +0200

    added output

--- experiments/reranker/train_reranker.py
+++ experiments/reranker/train_reranker.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from transformers import TFAutoModelForSequenceClassification
 from transformers import HfArgumentParser, TFTrainingArguments
 from dataclasses import dataclass, field
-
+import json
 # region Helper classes
 class SavePretrainedCallback(tf.keras.callbacks.Callback):
     # Hugging Face models have a save_pretrained() method that saves both the weights and the necessary
@@ -37,6 +37,18 @@ def _parse_function(example_proto):
     return {'input_ids': input_ids, 'attention_mask': attention_mask,
             'token_type_ids': token_type_ids},labels
 
+def create_training_dataset(data_args,training_args):
+    train_files = tf.io.gfile.glob(data_args.train_files)
+    raw_train_data = tf.data.TFRecordDataset(train_files, num_parallel_reads=tf.data.AUTOTUNE)
+    parsed_train_dataset = raw_train_data.map(_parse_function)
+    train_dataset = parsed_train_dataset.shuffle(buffer_size=len(parsed_train_dataset))
+    if training_args.max_steps > -1:
+        max_train_size = training_args.max_steps * training_args.train_batch_size
+        print("number of train samples:", max_train_size)
+        train_dataset = train_dataset.take(max_train_size)
+    train_dataset.batch(training_args.train_batch_size)
+    return train_dataset
+
 @dataclass
 class DataArguments:
     train_files:str = "/v/tomergur/convo/ms_marco/records_dev_only_q/*.tfrecords"
@@ -54,13 +66,6 @@ if __name__ == "__main__":
         print(t)
     '''
     with training_args.strategy.scope():
-        train_files = tf.io.gfile.glob(data_args.train_files)
-        raw_train_data = tf.data.TFRecordDataset(train_files,num_parallel_reads=tf.data.AUTOTUNE)
-        parsed_train_dataset = raw_train_data.map(_parse_function)
-        if training_args.max_steps > -1:
-            max_train_size = training_args.max_steps * training_args.train_batch_size
-            print("number of train samples:",max_train_size)
-            train_dataset = parsed_train_dataset.take(max_train_size)
             #validation_dataset = parsed_train_dataset.skip(max_train_size)
         model = TFAutoModelForSequenceClassification.from_pretrained(model_name, type_vocab_size=2,from_pt=True)
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
@@ -68,16 +73,22 @@ if __name__ == "__main__":
         # metrics = metrics
         model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=training_args.learning_rate), loss=loss,metrics = metrics)
         if training_args.do_train:
+            train_dataset=create_training_dataset(data_args,training_args)
             #, callbacks=callbacks
             callbacks = [SavePretrainedCallback(output_dir=training_args.output_dir)]
-            history=model.fit(train_dataset.batch(training_args.train_batch_size),
-                      epochs=int(training_args.num_train_epochs), verbose=1)
+            history=model.fit(train_dataset,epochs=int(training_args.num_train_epochs), verbose=1)
             print(history.history)
         if training_args.do_eval:
             test_files = tf.io.gfile.glob(data_args.test_files)
             raw_test_data = tf.data.TFRecordDataset(test_files, num_parallel_reads=tf.data.AUTOTUNE)
             test_dataset = raw_test_data.map(_parse_function)
+            '''
+            for t in test_dataset.take(10).batch(2):
+                print(t)
+            '''
             print("eval model!!!")
             #training_args.eval_steps
-            res = model.evaluate(test_dataset.take(10*training_args.eval_batch_size).batch(training_args.eval_batch_size))
+            res = model.evaluate(test_dataset.take(10*training_args.eval_batch_size).batch(training_args.eval_batch_size),return_dict=True)
             print("eval res:", res)
+            with open("{}/eval_res.json".format(training_args.output_dir),'w') as f:
+                json.dump(res,f)
