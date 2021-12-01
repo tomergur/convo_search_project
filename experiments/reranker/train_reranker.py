e2521e3e57baf0f0fa38595e8910506d64cba393
yomer <tomerg400@gmail.com>
2021-12-01 13:21:43 +0200

    try to work with keras model

--- experiments/reranker/train_reranker.py
+++ experiments/reranker/train_reranker.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
-from transformers import TFAutoModelForSequenceClassification,AutoTokenizer
+from transformers import TFAutoModelForSequenceClassification, AutoTokenizer
 from transformers import HfArgumentParser, TFTrainingArguments
 from dataclasses import dataclass, field
 import json
 import os
+
+
 # region Helper classes
 class SavePretrainedCallback(tf.keras.callbacks.Callback):
     # Hugging Face models have a save_pretrained() method that saves both the weights and the necessary
@@ -14,9 +16,8 @@ class SavePretrainedCallback(tf.keras.callbacks.Callback):
         self.output_dir = output_dir
 
     def on_epoch_end(self, epoch, logs=None):
-        #self.model.save_weights("{}/tpu-model.h5".format(self.output_dir))
+        # self.model.save_weights("{}/tpu-model.h5".format(self.output_dir))
         self.model.save_pretrained(self.output_dir)
-
 
 
 FEATURE_DESC = {
@@ -30,35 +31,49 @@ FEATURE_DESC = {
 def _parse_function(example_proto):
     # Parse the input `tf.train.Example` proto using the dictionary above.
     example = tf.io.parse_single_example(example_proto, FEATURE_DESC)
-    input_ids=tf.cast(example['input_ids'],tf.int32)
-    attention_mask=tf.cast(example['input_ids'],tf.int32)
+    input_ids = tf.cast(example['input_ids'], tf.int32)
+    attention_mask = tf.cast(example['input_ids'], tf.int32)
     attention_mask = tf.cast(example['attention_mask'], tf.int32)
     token_type_ids = tf.cast(example['token_type_ids'], tf.int32)
-    labels=tf.cast(example['labels'], tf.int32)
+    labels = tf.cast(example['labels'], tf.int32)
     return {'input_ids': input_ids, 'attention_mask': attention_mask,
-            'token_type_ids': token_type_ids},labels
+            'token_type_ids': token_type_ids}, labels
 
-def create_training_dataset(data_args,training_args):
+
+def create_training_dataset(data_args, training_args):
     train_files = tf.io.gfile.glob(data_args.train_files)
     raw_train_data = tf.data.TFRecordDataset(train_files, num_parallel_reads=tf.data.AUTOTUNE)
     parsed_train_dataset = raw_train_data.map(_parse_function)
-    train_dataset=parsed_train_dataset
-    #train_dataset = parsed_train_dataset.shuffle(buffer_size=len(parsed_train_dataset))
+    train_dataset = parsed_train_dataset
+    # train_dataset = parsed_train_dataset.shuffle(buffer_size=len(parsed_train_dataset))
     if training_args.max_steps > -1:
         max_train_size = training_args.max_steps * training_args.train_batch_size
         print("number of train samples:", max_train_size)
         train_dataset = train_dataset.take(max_train_size)
-    train_dataset=train_dataset.batch(training_args.train_batch_size)
+    train_dataset = train_dataset.batch(training_args.train_batch_size)
     return train_dataset
+
+
+def create_model(model_name):
+    model = TFAutoModelForSequenceClassification.from_pretrained(model_name, type_vocab_size=2, from_pt=True)
+    input_ids = tf.keras.layers.Input(shape=(512,), name='input_ids', dtype='int32')
+    att_mask = tf.keras.layers.Input(shape=(512,), name='attention_mask', dtype='int32')
+    tt_ids = tf.keras.layers.Input(shape=(512,), name='token_type_ids', dtype='int32')
+    logits = model(input_ids, token_type_ids=tt_ids, attention_mask=att_mask, return_dict=False,
+                   training=False)
+    return tf.keras.Model(inputs=(input_ids, att_mask, tt_ids), outputs=logits)
+    # return model
+
 
 @dataclass
 class DataArguments:
-    train_files:str = "/v/tomergur/convo/ms_marco/records_dev_only_q/*.tfrecords"
-    test_files:str ="/v/tomergur/convo/ms_marco/records_dev_only_q/*.tfrecords"
+    train_files: str = "/v/tomergur/convo/ms_marco/records_dev_only_q/*.tfrecords"
+    test_files: str = "/v/tomergur/convo/ms_marco/records_dev_only_q/*.tfrecords"
+
 
 if __name__ == "__main__":
-    parser = HfArgumentParser((DataArguments,TFTrainingArguments))
-    data_args,training_args= parser.parse_args_into_dataclasses()
+    parser = HfArgumentParser((DataArguments, TFTrainingArguments))
+    data_args, training_args = parser.parse_args_into_dataclasses()
     # Create a description of the features.
     model_name = "castorini/monobert-large-msmarco-finetune-only"
     # model_name="bert-base-uncased"
@@ -71,17 +86,20 @@ if __name__ == "__main__":
     if not os.path.exists(training_args.output_dir):
         os.mkdir(training_args.output_dir)
     with training_args.strategy.scope():
-            #validation_dataset = parsed_train_dataset.skip(max_train_size)
-        model = TFAutoModelForSequenceClassification.from_pretrained(model_name, type_vocab_size=2,from_pt=True)
+        # validation_dataset = parsed_train_dataset.skip(max_train_size)
+        model = create_model(model_name)
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         metrics = ['accuracy']
         # metrics = metrics
-        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=training_args.learning_rate), loss=loss,metrics = metrics)
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=training_args.learning_rate), loss=loss,
+                      metrics=metrics)
         if training_args.do_train:
-            train_dataset=create_training_dataset(data_args,training_args)
+            train_dataset = create_training_dataset(data_args, training_args)
             callbacks = [SavePretrainedCallback(output_dir=training_args.output_dir)]
-            history=model.fit(train_dataset,epochs=int(training_args.num_train_epochs), verbose=1,callbacks=callbacks)
-            tokenizer=AutoTokenizer.from_pretrained(model_name)
+            callbacks = []
+            history = model.fit(train_dataset, epochs=int(training_args.num_train_epochs), verbose=1,
+                                callbacks=callbacks)
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
             tokenizer.save_pretrained(training_args.output_dir)
             print(history.history)
         if training_args.do_eval:
@@ -93,12 +111,12 @@ if __name__ == "__main__":
                 print(t)
             '''
             print("eval model!!!")
-            #training_args.eval_steps
-            #test_dataset.take(10 * training_args.eval_batch_size)
-            if training_args.eval_steps>-1:
-                max_eval_samples=training_args.eval_steps * training_args.eval_batch_size
-                test_dataset=test_dataset.take(max_eval_samples)
-            res = model.evaluate(test_dataset.batch(training_args.eval_batch_size),return_dict=True)
+            # training_args.eval_steps
+            # test_dataset.take(10 * training_args.eval_batch_size)
+            if training_args.eval_steps > -1:
+                max_eval_samples = training_args.eval_steps * training_args.eval_batch_size
+                test_dataset = test_dataset.take(max_eval_samples)
+            res = model.evaluate(test_dataset.batch(training_args.eval_batch_size), return_dict=True)
             print("eval res:", res)
-            with open("{}/{}_eval_res.json".format(training_args.output_dir,training_args.run_name),'w') as f:
-                json.dump(res,f)
+            with open("{}/{}_eval_res.json".format(training_args.output_dir, training_args.run_name), 'w') as f:
+                json.dump(res, f)
