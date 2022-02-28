5744fa57f8d2fb622d4dfebb994d9bb263f20b79
tomer <tomerg400@gmail.com>
2022-02-28 12:11:01 +0200

    add writing files

--- experiments/reranker/checkpoint_to_model.py
+++ experiments/reranker/checkpoint_to_model.py
@@ -10,13 +10,21 @@ if __name__=="__main__":
     args=parser.parse_args()
     model_name_or_path="castorini/monobert-large-msmarco-finetune-only"
     input_path=args.input_path
+    output_path=args.output_path
     checkpoints_dirs=tf.io.gfile.glob(input_path)
     print(checkpoints_dirs)
     tokenizer=AutoTokenizer.from_pretrained(model_name_or_path)
     model = TFAutoModelForSequenceClassification.from_pretrained(model_name_or_path, from_pt=True, num_labels=2)
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
     for checkpoint_dir in checkpoints_dirs:
-        checkpoint_num=checkpoint_dir.split("checkpoint")[1]
+        checkpoint_num=checkpoint_dir.split("checkpoint")[-1]
         print(checkpoint_num)
         model.load_weights(checkpoint_dir+"/")
+        output_dir="{}/checkpoint{}/".format(output_path,checkpoint_num)
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        tokenizer.save_pretrained(output_dir)
+        model.save_pretrained(output_dir)
 
 
