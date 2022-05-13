d699de8d33041e5f8ef41c4e116cd880682a7c4e
tomer <tomerg400@gmail.com>
2022-05-13 22:27:14 +0300

    verify doc modify max value is 5

--- convo_search_project/doc_modify.py
+++ convo_search_project/doc_modify.py
@@ -1,9 +1,9 @@
 from pygaggle.rerank.base import Text
-
+MAX_Q=5
 def modify_to_all_queries(doc_q,hits):
     texts = []
     for i,hit in enumerate(hits):
-        t =  "[SEP]".join(doc_q[hit.docid])
+        t =  "[SEP]".join(doc_q[hit.docid][:MAX_Q])
         metadata = {'raw': hit.raw, 'docid': hit.docid}
         texts.append(Text(t, metadata, hit.score))
     return texts
@@ -11,7 +11,7 @@ def modify_to_all_queries(doc_q,hits):
 def modify_to_append_all_queries(doc_q,hits):
     texts = []
     for i,hit in enumerate(hits):
-        t =  "[SEP]".join([hit.raw]+doc_q[hit.docid])
+        t =  "[SEP]".join([hit.raw]+doc_q[hit.docid][:MAX_Q])
         metadata = {'raw': hit.raw, 'docid': hit.docid}
         texts.append(Text(t, metadata, hit.score))
     return texts
